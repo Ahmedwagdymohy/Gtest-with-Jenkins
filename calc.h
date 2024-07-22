a7ca76f925654160a9f4040d73c062b444181ee1
Ahmed-Wagdy-Mohy <62253674+Ahmedwagdymohy@users.noreply.github.com>
2024-07-22 23:42:19 +0300

    Add files via upload

---
+++ calc.h
@@ -0,0 +1,14 @@
+#ifndef CALC_H
+#define CALC_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+int add(int a, int b);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // CALC_H
---
+++ test_calc.cpp
@@ -0,0 +1,12 @@
+#include "gtest/gtest.h"
+#include "calc.h"
+  // Ensure this matches the location of your header file for the functions you want to test
+
+// Test case for add function
+TEST(AdditionTest, PositiveNumbers) {
+    EXPECT_EQ(add(1, 1), 2);
+    EXPECT_EQ(add(2, 3), 5);
+    EXPECT_EQ(add(2, 8), 15);
+}
+
+
