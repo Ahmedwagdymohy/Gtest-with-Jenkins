8ae9e686130926f81f3231843d1e003920d63ab6
Ahmed-Wagdy-Mohy <62253674+Ahmedwagdymohy@users.noreply.github.com>
2024-07-28 12:02:31 +0300

    Updating the test files

--- calc.c
+++ calc.c
@@ -1,14 +1,5 @@
 #include "calc.h"
-int add(int a, int b) {
-    if(a > b){
-    return a;
-    }else if(a < b){
-        return b;
-    }   
-    else if(a == b){
-    return 5;
-    }else{
-        return 0;
-    }
-}
 
+int add(int a, int b) {
+    return a + b;
+}
--- calc.h
+++ calc.h
@@ -1,14 +1,6 @@
 #ifndef CALC_H
 #define CALC_H
 
-#ifdef __cplusplus
-extern "C" {
-#endif
-
 int add(int a, int b);
-
-#ifdef __cplusplus
-}
-#endif
 
 #endif // CALC_H
